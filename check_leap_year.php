f56baef74129536705e96ad69e05606652b5029a
Abdulmalik <abdulmaliklawal388@gmail.com>
2025-02-04 22:11:03 +0100

    Update check_leap_year.php

--- check_leap_year.php
+++ check_leap_year.php
@@ -19,19 +19,14 @@
     
 <center>
 <?php
-function is_leap_year($year) {
-    // Check if the year is divisible by 4
-    if ($year % 4 == 0) {
-        // Check if it's divisible by 100
-        if ($year % 100 == 0) {
-            // Check if it's divisible by 400
-            if ($year % 400 == 0) {
+function is_leap_Year($year) {
+    if ($year % 400 == 0 || ($year % 4 == 0 && $year % 100 != 0)) {
                 return true;
             } else {
                 return false;
             }
         } 
-    } }
+    
 
 // Check if the form has been submitted and process the input
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
